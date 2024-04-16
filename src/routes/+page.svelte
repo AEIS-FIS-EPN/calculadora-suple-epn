2f8a414d7ef1bd21254b879594d73330d675cb18
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2024-04-15 23:23:48 -0500

    style: adjust styles for mobile landscape mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	import Footer from "../components/footer.svelte";
 </script>
 
-<div class="container flex flex-col items-center gap-y-8 pt-12 md:min-h-screen md:pt-20">
+<div class="container flex flex-col items-center gap-y-8 py-12 md:min-h-screen md:pt-20">
 	<Heading />
 	<Form />
 	<Footer />
